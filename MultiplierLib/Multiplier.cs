a49542dd8c5d3421ba84ce50af6954713274e20f
Henga1 <31842927+Henga1@users.noreply.github.com>
2017-09-11 11:27:38 +0200

    First push

---
+++ MultiplierLib/Multiplier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplierLib
+{
+    public class Multiplier
+    {
+        public long Simple(long l1, long l2)
+        {
+            return l1 * l2;
+        }
+
+        public object UsingFor(int v1, int v2)
+        {
+            return 0;
+        }
+    }
+}
---
+++ MultiplierTestsLib/MultiplierTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MultiplierLib;
+
+namespace MultiplierTestsLib
+{
+    [TestClass]
+    public class MultiplierTests
+    {
+        [TestMethod]
+        public void Simple()
+        {
+            Multiplier sut = new Multiplier();
+            var actual = sut.Simple(20, 20);
+            Assert.AreEqual(400, actual);
+        }
+        [TestMethod]
+        public void UsingFor()
+        {
+            Multiplier sut = new Multiplier();
+            var actual = sut.UsingFor(20, 20);
+            Assert.AreEqual(400, actual);
+        }
+    }
+}
